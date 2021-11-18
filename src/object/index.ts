07e2b7d0146dc9e5b0798e24717220898aef704d
41RDG6BR <rdg6design@gmail.com>
2021-11-18 13:47:17 -0300

    Tipo object

--- src/object/index.ts
+++ src/object/index.ts
@@ -3,17 +3,24 @@
 //   chaveB: 'Valor B'
 // };
 
+// const objetoA: {
+//   chaveA: string;
+//   chaveB: string;
+//   chaveC?: string;
+//   [key: string]: unknown;
+// } = {
+//   chaveA: 'Valor A',
+//   chaveB: 'Valor B'
+// };
+
 const objetoA: {
-  chaveA: string;
+  readonly chaveA: string;
   chaveB: string;
-  chaveC?: string;
-  [key: string]: unknown;
 } = {
   chaveA: 'Valor A',
   chaveB: 'Valor B'
 };
 
-objetoA.chaveA = 'Outro valor';
-objetoA.chaveC = 'Nova Chave'
-objetoA.chaveD = 'Nova Chave'
+objetoA.chaveB = 'Outro valor';
+
 console.log(objetoA)
